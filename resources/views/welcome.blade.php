ad7e8e16c943163ec377d6391d71debe3aa8322a
Ruma <matteoh8@hotmail.it>
2024-10-12 17:54:26 +0200

    hello world

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,19 +8,9 @@
     <title>Livewire Basiscs</title>
 </head>
 
-<body>
-    <div id="app"
-        class="container d-flex justify-content-center align-items-center mx-auto my-5 flex-column p-0 text-center">
-        <h1>Title</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident dolorum aliquam nostrum unde nobis illum
-            illo sunt a! Provident minus quidem tempora beatae est expedita quisquam temporibus voluptate fugiat
-            incidunt.
-            Culpa tenetur impedit officiis fugit obcaecati quam ducimus rerum. Voluptatem laboriosam consequatur quos
-            aperiam assumenda voluptate fugiat eveniet tempore, ut dolore placeat maiores deserunt saepe quam
-            repellendus, rerum fugit aspernatur?
-            Tempore, similique. Eos adipisci, voluptates eaque quasi unde amet quas! Delectus consectetur animi fugiat
-            quam illo. Esse nobis hic accusamus odit facere. Maxime, quia. Ut odit officia praesentium alias facere.</p>
-    </div>
-</body>
+    <body>
+        <livewire:hello-world />
+        @livewire('hello-world')
+    </body>
 
 </html>
