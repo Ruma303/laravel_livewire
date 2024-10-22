5b8b23b98b08fae34c858085f3244d2c2d80fea2
Ruma <matteoh8@hotmail.it>
2024-10-22 23:28:01 +0200

    fino a updated()

--- app/Livewire/Todos.php
+++ app/Livewire/Todos.php
@@ -3,25 +3,39 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+//use App\Models\Todo;
 
 class Todos extends Component
 {
-    public $todos = [
-        'Learn Laravel',
-        'Discover Livewire',
-        'Use Alpine.js'
-    ];
-
+    public $todos = [];
     public $todo = "";
+
+    public function mount()
+    {
+        //$this->todos = Todo::all();
+    }
+
+
+    public function updated($props, $value)
+    {
+        if ($props == "todo")
+            $this->$props = strtoupper($value);
+
+        //dd($props, strtoupper($value));
+    }
+
+    public function updatedTodo($value)
+    {
+            $this->todo = strtoupper($value);
+
+        //dd($props, strtoupper($value));
+    }
 
     public function add()
     {
         $this->todos[] = $this->todo;
-        //$this->todo = '';
         $this->reset('todo');
     }
-
-
 
     public function render()
     {
