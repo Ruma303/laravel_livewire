44f4f8628c794b235c259219c72ff45ba8a70d57
Ruma <110823021+Ruma303@users.noreply.github.com>
2024-12-02 09:11:43 +0000

    da aggiustare

--- resources/views/livewire/posts/index.blade.php
+++ resources/views/livewire/posts/index.blade.php
@@ -1,7 +1,11 @@
 <div class="overflow-x-auto relative w-full">
     <nav class="flex flex-row space-x-4 mb-5">
-        <button type="button" wire:click='generate_posts()' class="text-white p-1 bg-emerald-500 rounded-md hover:scale-105 hover:transition-all mb-2 shadow-md shadow-green-200 h-fit">Quick Generate Posts</button>
-        <a type="button" href="{{ route('posts.create') }}" class="text-white p-1 bg-violet-500 rounded-md hover:scale-105 hover:transition-all mb-2 shadow-md shadow-violet-200 h-fit">Create Posts</a>
+        <button type="button" wire:click='generate_posts()'
+            class="text-white p-1 bg-emerald-500 rounded-md hover:scale-105 hover:transition-all mb-2 shadow-md shadow-green-200 h-fit">Quick
+            Generate Posts</button>
+        <a type="button" href="{{ route('posts.create') }}"
+            class="text-white p-1 bg-violet-500 rounded-md hover:scale-105 hover:transition-all mb-2 shadow-md shadow-violet-200 h-fit">Create
+            Posts</a>
         <livewire:posts.search :results="$results" />
     </nav>
     <div class="shadow-md shadow-violet-300 overflow-hidden border-b border-violet-200 sm:rounded-lg">
@@ -29,7 +33,10 @@
                 </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
+                @dump($posts)
+                @dump($results)
                 @foreach ($results as $post)
+                    @dump($post)
                     <tr wire:key='"{{ $post->id }}'>
                         <td class="px-6 py-4 whitespace-nowrap flex flex-wrap gap-5">
                             <button type="button" wire:click='delete({{ $post->id }})'
